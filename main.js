a24b3974cc856ce72899a06b8f304a7a033dcdc6
Stacey Vanyo <sbvanyo@gmail.com>
2023-05-04 15:45:42 -0500

    color changing cards and some other css stuff

--- main.js
+++ main.js
@@ -4,32 +4,33 @@ console.log("testing, testing");
 const students = [
   {
     id: 1,
-    name: "Bellatrix",
-    house: "Slytherin",
-    image: "https://images.unsplash.com/photo-1500373994708-4d781bd7bd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    name: "Chocolate Frog",
+    house: "SLYTHERIN",
+    image: "https://i.ytimg.com/vi/nV9IEQbRufE/maxresdefault.jpg"
   },
   {
     id: 2,
-    name: "Buckbeak",
-    house: "Gryffindor",
+    name: "Hedwig",
+    house: "GRYFFINDOR",
     image: "https://images.unsplash.com/photo-1500373994708-4d781bd7bd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
   },
   {
     id: 3,
     name: "Fluffy",
-    house: "Ravenclaw",
-    image: "https://images.unsplash.com/photo-1500373994708-4d781bd7bd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    house: "RAVENCLAW",
+    image: "https://images.saymedia-content.com/.image/t_share/MTc2Mjk0MjYxMjIxMjM3OTMz/harry-potter-fluffy.jpg"
   },
   {
     id: 4,
-    name: "Filch",
-    house: "Hufflepuff",
-    image: "https://images.unsplash.com/photo-1500373994708-4d781bd7bd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    name: "Argus Filch",
+    house: "HUFFLEPUFF",
+    image: "https://pm1.narvii.com/6133/37385c3eed76a1accda8a88bf29b44a11ed0a621_00.jpg"
   }
 ];
 
 //EXPELLED ARRAY
-let expelled = [];
+const expelledStudents = [];
+
 
 //RENDER TO DOM UTILITY FUNCTION
 const renderToDom = (divId, htmlToRender) => {
@@ -42,13 +43,14 @@ const renderToDom = (divId, htmlToRender) => {
 const cardsOnDom = (array) => {
   let domString = "";
   for (const student of array) {
-    domString += `<div class="card" style="width: 18rem;">
+
+    domString += `<div class="card" id="${student.house}-card" style="width: 18rem;">
     <img src="${student.image}" class="card-img-top" alt="placeholder hedwig">
     <div class="card-header">${student.house}!</div>
     <div class="card-body">
       <h5 class="card-title">${student.name}</h5>
       <p class="card-text">"wingardium leviosa"</p>
-      <button class="btn btn-danger" id="delete--${student.id}">Expel</button>
+      <button class="btn btn-danger" id="expel--${student.id}">Expel</button>
     </div>
   </div>`
   }
@@ -75,12 +77,56 @@ const formOnDom = () => {
 
 
 //HOUSE RANDOMIZER//
-const gryffindor = 0;
-const slytherin = 1;
-const hufflepuff = 2;
-const ravenclaw = 3;
+const houseRandomizer = () => {
 
-const houseRandomizer = Math.floor(Math.random() * 4);
+  const house = Math.floor(Math.random() * 4);
+
+  switch (house) {
+    case 0:
+      return "GRYFFINDOR";
+      break;
+    case 1:
+      return "SLYTHERIN";
+      break;
+    case 2:
+      return "HUFFLEPUFF";
+      break;
+    case 3:
+      return "RAVENCLAW";
+      break;
+  };
+
+  return house;
+
+}
+
+
+
+
+
+
+// //IMAGE RANDOMIZER
+// const imageRandomizer = () => {
+
+//   switch (newStudentObj.house) {
+//     case "GRYFFINDOR":
+//       return `house crests/gryffindor-crest.jpg`;
+//       break;
+//     case "SLYTHERIN":
+//       return `house crests/slytherin-crest.jpg`;
+//       break;
+//     case 2: "HUFFLEPUFF"
+//       return `house crests/hufflepuff-crest.jpg`;
+//       break;
+//     case 3: "RAVENCLAW"
+//       return `house crests/ravenclaw-crest.jpg`;
+//       break;
+//   };
+
+// }
+
+
+
 
 
 
@@ -98,42 +144,93 @@ const newStudent = (e) => {
   const newStudentObj = {
     id: students.length + 1,
     name: document.querySelector("#name-input").value,
-    house: houseRandomizer,
-    image: "https://cdna.artstation.com/p/assets/images/images/004/163/098/large/sam-rowan-boswell-niffler-v001-009-sr.jpg?1480958484&dl=1"
+    house: houseRandomizer(),
+    image: "https://images.unsplash.com/photo-1500373994708-4d781bd7bd15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
   }
-
 
 students.unshift(newStudentObj);
 cardsOnDom(students);
 document.querySelector("form").reset();
 
+
 };
 
 
-
 form.addEventListener('submit', newStudent);
+
+
+
+
 
 
 // ******************** //
 // ****** DELETE ****** //
 // ******************** //
 
-// 1. Target the sorted cards div
-const sorted = document.querySelector("#sorted-cards");
 
-// 2. Add an event listener to capture clicks
-sorted.addEventListener('click', (e) => {
-// 3. check e.target.id includes "delete"
-  if(e.target.id.includes("delete")) {
-    const [, id] = e.target.id.split("--");
-// 4. add logic to remove from array
-    const index = students.findIndex(student => student.id === Number(id));
-  //.splice modifies the original array
-    students.splice(index, 1);
-// 5. Repaint the DOM with the updated array
-    cardsOnDom(students);
+
+//GET EXPELLED STUDENT CARDS ON DOM
+const expelledCardsOnDom = (array) => {
+  let domString = "";
+  for (const student of array) {
+    domString += `<div class="card" style="width: 18rem;">
+    <img src="${student.image}" class="card-img-top" alt="placeholder hedwig">
+    <div class="card-header">${student.house}!</div>
+    <div class="card-body">
+      <h5 class="card-title">${student.name}</h5>
+      <p class="card-text">"wingardium leviosa"</p>
+    </div>
+  </div>`
   }
-})
+
+  renderToDom("#expelled-cards", domString);
+};
+
+
+//EXPEL A STUDENT
+const expelStudent = (event) => {
+  //if the id includes "expel"
+  if (event.target.id.includes("expel")) {
+    // get that object id off of our target ID
+    const [, studentId] = event.target.id.split("--");
+    // Use it to find the index of the object
+    const studentIndex = students.findIndex(
+      (student) => Number(studentId) === student.id
+    );
+
+    // splice that object out of the array
+    const expelledStudent = students.splice(studentIndex, 1);
+
+    // push our student into the expelledStudents array
+    expelledStudents.push(expelledStudent);
+
+    // Render both of our arrays! Expelled and regular.
+      expelledCardsOnDom(expelledStudent);
+      cardsOnDom(students);
+  }
+
+};
+
+
+
+
+
+// // 1. Target the sorted cards div
+// const sorted = document.querySelector("#sorted-cards");
+
+// // 2. Add an event listener to capture clicks
+// sorted.addEventListener('click', (e) => {
+// // 3. check e.target.id includes "delete"
+//   if(e.target.id.includes("delete")) {
+//     const [, id] = e.target.id.split("--");
+// // 4. add logic to remove from array
+//     const index = students.findIndex(student => student.id === Number(id));
+//   //.splice modifies the original array
+//     students.splice(index, 1);
+// // 5. Repaint the DOM with the updated array
+//     cardsOnDom(students);
+//   }
+// })
 
 // //GET EXPELLED CARDS ON DOM
 // const expelledOnDom = (array) => {
@@ -221,21 +318,21 @@ showAll.addEventListener('click', () => {
 
 // 3. Add click event to filter by house on button click
 showGryffindor.addEventListener('click', () => {
-  const gryffindor = filter(students, "Gryffindor");
+  const gryffindor = filter(students, "GRYFFINDOR");
   cardsOnDom(gryffindor);
 })
 
 showHufflepuff.addEventListener('click', () => {
-  const hufflepuff = filter(students, "Hufflepuff");
+  const hufflepuff = filter(students, "HUFFLEPUFF");
   cardsOnDom(hufflepuff);
 })
 
 showRavenclaw.addEventListener('click', () => {
-  const ravenclaw = filter(students, "Ravenclaw");
+  const ravenclaw = filter(students, "RAVENCLAW");
   cardsOnDom(ravenclaw);
 })
 
 showSlytherin.addEventListener('click', () => {
-  const slytherin = filter(students, "Slytherin");
+  const slytherin = filter(students, "SLYTHERIN");
   cardsOnDom(slytherin);
 })
